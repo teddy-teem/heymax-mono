340ee7e5e4da57656249ceea92d4f11e24803c8a
teddy-teem <jahidrsn@gmail.com>
2025-02-12 00:27:37 +0600

    chore: Eslint added.

--- packages/assets/index.ts
+++ packages/assets/index.ts
@@ -1,28 +1,28 @@
-import { Platform } from "react-native";
+import { Platform } from 'react-native';
 
-const isWeb = Platform.OS === "web";
+const isWeb = Platform.OS === 'web';
 
 export const icons = {
   FlyClaimIcon: isWeb
-    ? "/icons/fly-claim.png"
-    : require("./icons/fly-claim.png"),
+    ? '/icons/fly-claim.png'
+    : require('./icons/fly-claim.png'),
   NoFlightSoonIcon: isWeb
-    ? "/icons/no-flight-soon.png"
-    : require("./icons/no-flight-soon.png"),
+    ? '/icons/no-flight-soon.png'
+    : require('./icons/no-flight-soon.png'),
   TransferMilesIcon: isWeb
-    ? "/icons/transfer-miles.png"
-    : require("./icons/transfer-miles.png"),
+    ? '/icons/transfer-miles.png'
+    : require('./icons/transfer-miles.png'),
   RightArrowIcon: isWeb
-    ? "/icons/transfer-miles.png"
-    : require("./icons/transfer-miles.png"),
+    ? '/icons/transfer-miles.png'
+    : require('./icons/transfer-miles.png'),
 };
 
 export const images = {
-  AmazonSGImage: require("./images/amazon.sg.png"),
-  FairPriceImage: require("./images/fair-price.png"),
-  FaveImage: require("./images/fave.png"),
-  HnMImage: require("./images/HnM.png"),
-  LazadaImage: require("./images/Lazada.png"),
-  MerchantImage: require("./images/merchant.png"),
-  ShopeeImage: require("./images/Shopee.png"),
+  AmazonSGImage: require('./images/amazon.sg.png'),
+  FairPriceImage: require('./images/fair-price.png'),
+  FaveImage: require('./images/fave.png'),
+  HnMImage: require('./images/HnM.png'),
+  LazadaImage: require('./images/Lazada.png'),
+  MerchantImage: require('./images/merchant.png'),
+  ShopeeImage: require('./images/Shopee.png'),
 };
--- packages/constants/src/index.ts
+++ packages/constants/src/index.ts
@@ -1,42 +1,42 @@
-export const LAST_SCREEN_REDIRECT_URL = "https://heymax.ai";
-import { icons, images } from "@heymax/assets";
+export const LAST_SCREEN_REDIRECT_URL = 'https://heymax.ai';
+import { icons, images } from '@heymax/assets';
 
 export const ROUTES = [
-  { route: "/", id: 0, instruction: "See How It Works" },
-  { route: "/shopping", id: 1, instruction: "" },
-  { route: "/redeem", id: 2, instruction: "Get Started" },
+  { route: '/', id: 0, instruction: 'See How It Works' },
+  { route: '/shopping', id: 1, instruction: '' },
+  { route: '/redeem', id: 2, instruction: 'Get Started' },
 ];
 
 export const REDEEMS = [
   {
     id: 1,
-    title: "Transfer miles",
-    body: "25 Airline and Hotel Partners, No Fees, 1:1 Transfer Ratio",
+    title: 'Transfer miles',
+    body: '25 Airline and Hotel Partners, No Fees, 1:1 Transfer Ratio',
     icon: icons.TransferMilesIcon,
-    instruction: "Best for Business & First Class",
+    instruction: 'Best for Business & First Class',
   },
   {
     id: 2,
-    title: "Fly and Claim",
-    body: "Upload your flight ticket, and get reimbursed within 5 days.",
+    title: 'Fly and Claim',
+    body: 'Upload your flight ticket, and get reimbursed within 5 days.',
     icon: icons.FlyClaimIcon,
-    instruction: "Best for Economy",
+    instruction: 'Best for Economy',
   },
   {
     id: 3,
-    title: "Not flying soon",
-    body: "You can always redeem Gift Cards with your Max Miles.",
+    title: 'Not flying soon',
+    body: 'You can always redeem Gift Cards with your Max Miles.',
     icon: icons.NoFlightSoonIcon,
-    instruction: "Best for Starters",
+    instruction: 'Best for Starters',
   },
 ];
 
 export const QUICK_ACTIONS = [
-  "Shop Online",
-  "Book Travel",
-  "Order Food",
-  "Order Med",
-  "Trade Online",
+  'Shop Online',
+  'Book Travel',
+  'Order Food',
+  'Order Med',
+  'Trade Online',
 ];
 
 export const SHOPPING_BRANDS = [
